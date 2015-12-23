a38248f27a28dc73e17b7df88e3dfb358312c581
Tankred Hase <mail@tankredhase.de>
2015-12-23 16:50:44 +0700

    Implement Login with Google on first screen

--- src/js/controller/login/add-account.js
+++ src/js/controller/login/add-account.js
@@ -1,78 +1,21 @@
 'use strict';
 
-var AddAccountCtrl = function($scope, $location, $routeParams, $timeout, $q, mailConfig, auth, dialog) {
-    !$routeParams.dev && !auth.isInitialized() && $location.path('/'); // init app
+var AddAccountCtrl = function($scope, $location, $routeParams, $q, oauth, gmailClient, dialog) {
+    !$routeParams.dev && oauth.accessToken && $location.path('/'); // init app
 
-    $scope.getAccountSettings = function() {
-        if ($scope.form.$invalid) {
-            $scope.errMsg = 'Please enter a valid email address!';
-            return;
-        }
-
+    $scope.signInWithGoogle = function() {
         return $q(function(resolve) {
             $scope.busy = true;
             $scope.errMsg = undefined; // reset error msg
             resolve();
 
         }).then(function() {
-            return mailConfig.get($scope.emailAddress);
+            // TODO: remove test email address after testing
+            oauth._loginHint = 'safewithme.testuser@gmail.com';
+            // get email address and oauth token
+            return gmailClient.login();
 
-        }).then(function(config) {
-            $scope.busy = false;
-            $scope.state.login = {
-                mailConfig: config,
-                emailAddress: $scope.emailAddress
-            };
-
-            var hostname = config.imap.hostname;
-            if (auth.useOAuth(hostname)) {
-                // check for oauth support
-                return $scope.oauthPossible();
-            } else {
-                // use standard password login
-                return $scope.setCredentials();
-            }
-
-        }).catch(function() {
-            $scope.busy = false;
-            $scope.errMsg = 'Error fetching IMAP settings for that email address!';
-        });
-    };
-
-    $scope.oauthPossible = function() {
-        // ask user to use the platform's native OAuth api
-        return dialog.confirm({
-            title: 'Google Account Login',
-            message: 'You are signing into a Google account. Would you like to sign in with Google or just continue with a password login?',
-            positiveBtnStr: 'Google sign in',
-            negativeBtnStr: 'Password',
-            showNegativeBtn: true,
-            faqLink: 'https://github.com/whiteout-io/mail-html5/wiki/FAQ#how-does-sign-in-with-google-work',
-            callback: function(granted) {
-                if (granted) {
-                    // query oauth token
-                    return getOAuthToken();
-                } else {
-                    // use normal user/password login
-                    $scope.setCredentials();
-                }
-            }
-        });
-
-        function getOAuthToken() {
-            // fetches the email address from the chrome identity api
-            return auth.getOAuthToken().then(function() {
-                // continue to setting credentials
-                return $scope.setCredentials();
-
-            }).catch(dialog.error);
-        }
-    };
-
-    $scope.setCredentials = function() {
-        return $timeout(function() {
-            $location.path('/login-set-credentials');
-        });
+        }).catch(dialog.error);
     };
 };
 
